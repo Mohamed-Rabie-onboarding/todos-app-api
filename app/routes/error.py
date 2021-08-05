8b1fafaae623a8cee2c0a95d5d1ef5576fd8834a
Muhamed-Rabie <elmdarym@incubaid.com>
2021-08-05 11:37:28 +0200

    raise error in 404/405

--- app/routes/error.py
+++ app/routes/error.py
@@ -7,11 +7,11 @@ def errorRoutes(app: Bottle):
 
     @app.error(404)
     def page_not_found(err: HTTPError):
-        return 'Page Not Found!'
+        raise Error([system_error_item('Page Not Found!')])
 
     @app.error(405)
     def method_not_allowed(err: HTTPError):
-        return 'Method Not Allowed'
+        raise Error([system_error_item('Method Not Allowed')])
 
     @app.error(500)
     def internal_server_error(err: HTTPError):
